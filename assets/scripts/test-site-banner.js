48d2e5b1694d2a95fee8bf3c86780778b5a2e687
andrewdp <andrew.depeiza@cra-arc.gc.ca>
2025-02-04 16:15:20 -0500

    improved URL generating scripts

--- assets/scripts/test-site-banner.js
+++ assets/scripts/test-site-banner.js
@@ -1,23 +1,46 @@
 "use strict";
 
-let githubURL,
+let exitPage = "exit-intent.html",
     getGithubURL = function (pageURL) {
-        var pageName = "";
+        let pageName = "",
+            githubURL = null;
+
         if (pageURL.indexOf(".htm") === -1) {
             pageName = "index.html";
-        };
-
-        if (pageURL.indexOf(".github.io/") > -1) {
-            return pageURL.toString().replace(/^https:\/\/(.*?)\.github\.io\/(.*?)\/((?:.*)(?=\/))?(\/?.*\.(?:[^\?#])*)?/, "https:\/\/github\.com\/$1\/$2\/blob\/main\/$3$4" + pageName);
-        } else if (pageURL.indexOf("://test.canada.ca/") > -1) {
-            return pageURL.toString().replace(/^https:\/\/test\.canada\.ca\/(.*?)\/((?:.*)(?=\/))?(\/?.*\.(?:[^\?#])*)?/, "https:\/\/github\.com\/gc-proto\/$1\/blob/master/$2$3" + pageName);
-        } else if (pageURL.indexOf(".alpha.canada.ca/") > -1) {
-            return pageURL.toString().replace(/^https:\/\/design\.cra-arc\.alpha\.canada\.ca\/(.*?)\/((?:.*)(?=\/))?(\/?.*\.(?:[^\?#])*)?/, "https:\/\/github\.com\/alpha-canada-ca\/cra-ucd-guide\/blob/main/$1\/$2$3" + pageName);
-            return "#";
         }
-        return "";
-    };
 
+        switch (true) {
+            // Generate github.com URL's from [repo name].github.io URL's
+            case pageURL.indexOf(".github.io/") > -1:
+                githubURL = pageURL.toString().replace(new RegExp("^https:\/\/(.*?)\.github\.io\/(.*?)\/((?:.*)(?=\/))?(\/?.*\..+)?"), "https:\/\/github\.com\/$1\/$2\/blob\/main\/$3$4" + pageName);
+                break;
+            // Generate github.com URL's from test.canada.ca URL's
+            case pageURL.indexOf("://test.canada.ca/") > -1:
+                githubURL = pageURL.toString().replace(new RegExp("^https:\/\/test\.canada\.ca\/(.*?)\/((?:.*)(?=\/))?(\/?.*\..+)?"), "https:\/\/github\.com\/gc-proto\/$1\/blob/master/$2$3" + pageName);
+                break;
+            // Generate github.com URL's from [repo name].alpha.canada.ca URL's
+            case pageURL.indexOf(".alpha.canada.ca/") > -1:
+                githubURL = pageURL.toString().replace(new RegExp("^https:\/\/design\.cra-arc\.alpha\.canada\.ca\/(.*?)\/((?:.*)(?=\/))?(\/?.*\..+)?"), "https:\/\/github\.com\/alpha-canada-ca\/cra-ucd-guide\/blob/main/$1\/$2$3" + pageName);
+                break;
+        }
+        return githubURL;
+    },
+    getDomain = function (url) {
+        let pattern = new RegExp("^(https?:\/\/[^\/]+\/[^\/]*\/?)"),
+            domains = pattern.exec(url);
+
+        return domains[0];
+    },
+    currentPageUrl = window.location.origin + window.location.pathname,
+    githubURL = getGithubURL(currentPageUrl),
+    rootDomain = getDomain(currentPageUrl),
+    visitedLinkStyle = document.createElement("style");
+
+//Remove visited link highlighting from links to exit page
+visitedLinkStyle.innerHTML = "a[href*='" + rootDomain + exitPage + "']:visited{ color:#284162;}";
+$("head").append(visitedLinkStyle);
+
+// Initalize Edit button
 document.getElementById("editBtn").addEventListener("click", function (event) {
     if (document.getElementsByTagName("main").contentEditable === "true") {
         document.getElementsByTagName("main").contentEditable = "false";
@@ -35,10 +58,24 @@ document.getElementById("editBtn").addEventListener("click", function (event) {
     event.preventDefault();
 });
 
-githubURL = getGithubURL(window.location.href);
-if (githubURL === "") {
-    document.getElementById("githubBtnGrp").classList.add("wb-inv");
-} else {
+// Initalize Github button
+if (githubURL !== null) {
+    document.getElementById("githubBtnGrp").classList.remove("wb-inv");
     document.getElementById("devtoolbar").classList.add("mrgn-rght-md");
     document.getElementById("githubBtn").href = githubURL;
 }
+
+$(document).on("wb-ready.wb", function () {
+    $("a:not([href^='mailto:'], [href^='#'], [href^='" + rootDomain + "'])").each(function updateExitURL() {
+        this.data("wb-exitscript", { url: rootDomain + exitPage });
+/*
+        this.href = rootDomain + exitPage + "?uri=" + this.href.replace("?", "&") + "&pagetitle=" + encodeURIComponent(this.innerText);
+        this.addClass("wb-exitscript");
+*/
+    });
+    $("form:not([action^='" + rootDomain + "'])").each(function updateExitURL() {
+/*
+        this.action = rootDomain + exitPage + "?uri=" + this.action.replace("?", "&");
+*/
+    });
+});
