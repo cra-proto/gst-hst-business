dd197f9403f16948eaa252238dbe1c3afc70945c
andrewdp <andrew.depeiza@cra-arc.gc.ca>
2025-04-25 16:13:59 -0400

    Refactored banner code

--- templates/scripts/test-site-banner.js
+++ templates/scripts/test-site-banner.js
@@ -10,7 +10,11 @@
 const devOptionsLocStore = localStorage.getItem("gitCRATemplateDevOptions");
 
 let devOptions = document.getElementById("devoptions");
-let getGithubURL = function (pageURL) {
+let keywords = document.getElementById("pageKeywords");
+let sourceUrlList = document.getElementById("sourceurl");
+
+let sourceUrlArr, 
+    getGithubURL = function (pageURL) {
         let pageName = "", 
             githubURL = null;
 
@@ -34,46 +38,52 @@ let getGithubURL = function (pageURL) {
         }
         if (githubURL !== null) {
             document.getElementById("githubBtnGrp").classList.remove("hide-devmenu");
-            document.getElementById("devtoolbar").classList.add("mrgn-rght-md");
             document.getElementById("githubBtn").href = githubURL;
         }
     };
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function initDevOpts() {
     if (devOptions.value.toLowerCase() === "true" || devOptionsLocStore.toLowerCase() === "true") {
-        // Add toolbar and buttons
-        // document.getElementById("test-banner").append("<div id=\"devtoolbar\" class=\"pull-right\"><ul class=\"btn-toolbar list-inline\" role=\"toolbar\"><li class=\"btn-group\"><a id=\"editBtn\" class=\"btn btn-default btn-sm\" data-exit=\"false\" href=\"\" title=\"Edit\"><span class=\"fa fa-edit\"></span><span class=\"wb-inv\">Edit</span></a></li><li id=\"githubBtnGrp\" class=\"btn-group hide-devmenu\"><a id=\"githubBtn\" class=\"btn btn-default btn-sm\" data-exit=\"false\" href=\"#\" title=\"Go to Github source\"><span class=\"fab fa-github\"></span><span class=\"wb-inv\">Go to Github source</span></a></li></ul></div>");
-
-         //       "<ol id=\"testpage-source\" class=\"list-inline mrgn-bttm-0\"></ol>";
-        // loop
-        // "<li><span class=\"glyphicon glyphicon-link\"></span>&nbsp;<a data-exit=\"false\" href=\"" + source.link "\">" + source.title + "</a></li>";
-
-        // document.getElementById("testpage-source").after("<p class=\"mrgn-bttm-0\"><span class=\"glyphicon glyphicon-tags mrgn-rght-sm\"><span class=\"wb-inv\">Keywords:</span></span>" + page.keywords + "</p>");
-         
-    if (document.getElementById("devtoolbar")) {
-        
-        // Initalize Edit button
-        document.getElementById("editBtn").addEventListener("click", function (event) {
-            if (document.getElementsByTagName("main").contentEditable === "true") {
-                document.getElementsByTagName("main").contentEditable = "false";
-                document.designMode = "off";
-                void 0;
-                // saves cuurent modified page content to local storage
-//                sessionStorage.setItem("content", document.getElementsByTagName("main")[0].innerHTML);
-                this.classList.add("btn-default");
-                this.classList.remove("btn-primary");
-            } else {
-                document.getElementsByTagName("main").contentEditable = "true";
-                document.designMode = "on";
-                void 0;
-                this.classList.add("btn-primary");
-                this.classList.remove("btn-default");
+        $(document).on("wb-ready.wb", function () {
+            // Add toolbar and buttons
+            document.getElementById("test-banner").innerHTML = "<div id=\"devtoolbar\" class=\"pull-right mrgn-rght-md\"><ul class=\"btn-toolbar list-inline\" role=\"toolbar\"><li class=\"btn-group\"><a id=\"editBtn\" class=\"btn btn-default btn-sm\" data-exit=\"false\" href=\"\" title=\"Edit\"><span class=\"fa fa-edit\"></span><span class=\"wb-inv\">Edit</span></a></li><li id=\"githubBtnGrp\" class=\"btn-group hide-devmenu\"><a id=\"githubBtn\" class=\"btn btn-default btn-sm\" data-exit=\"false\" href=\"#\" title=\"Go to Github source\"><span class=\"fab fa-github\"></span><span class=\"wb-inv\">Go to Github source</span></a></li></ul></div>" + document.getElementById("test-banner").innerHTML;
+            if (sourceUrlList !== null && sourceUrlList.value !== "") {
+                sourceUrlArr = JSON.parse(sourceUrlList.value);
+                if ((sourceUrlArr.length === 1 && sourceUrlArr[0].sourcetitle !== "") || sourceUrlArr.length > 1) {
+                    document.getElementById("test-banner-content").innerHTML = document.getElementById("test-banner-content").innerHTML + "<ol id=\"testpage-source\" class=\"list-inline mrgn-bttm-0\"></ol>";
+                    sourceUrlArr.forEach(function addSourceLinks(sourceUrlData) {
+                       document.getElementById("testpage-source").innerHTML = document.getElementById("testpage-source").innerHTML + "<li><span class=\"glyphicon glyphicon-link\"></span>&nbsp;<a data-exit=\"false\" href=\"" + sourceUrlData.sourcelink + "\">" + sourceUrlData.sourcetitle + "</a></li>";
+                    });
+                }
             }
-            event.preventDefault();
-        });
+            if (keywords !== null && keywords.value !== "") {
+                document.getElementById("test-banner-content").innerHTML = document.getElementById("test-banner-content").innerHTML + "<p class=\"mrgn-bttm-0\"><span class=\"glyphicon glyphicon-tags mrgn-rght-sm\"><span class=\"wb-inv\">Keywords:</span></span><span id=\"pageKeywords\" class=\"mrgn-lft-sm\">" + keywords.value + "</span></p>";
+            }
 
-        // Initalize Github button
-        getGithubURL(window.location.origin + window.location.pathname);
+            if (document.getElementById("devtoolbar") !== null) {
+        
+                // Initalize Edit button
+                document.getElementById("editBtn").addEventListener("click", function (event) {
+                    if (document.getElementsByTagName("main").contentEditable === "true") {
+                        document.getElementsByTagName("main").contentEditable = "false";
+                        document.designMode = "off";
+                        void 0;
+                        // saves cuurent modified page content to local storage
+//                sessionStorage.setItem("content", document.getElementsByTagName("main")[0].innerHTML);
+                        this.classList.add("btn-default");
+                        this.classList.remove("btn-primary");
+                    } else {
+                        document.getElementsByTagName("main").contentEditable = "true";
+                        document.designMode = "on";
+                        void 0;
+                        this.classList.add("btn-primary");
+                        this.classList.remove("btn-default");
+                    }
+                    event.preventDefault();
+                });
+
+                // Initalize Github button
+                getGithubURL(window.location.origin + window.location.pathname);
 
         // Load modied page content if it exists from local Storage
 /*
@@ -83,6 +93,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
 */
-    }
+            }
+        });
     }
 }, { once: true });
